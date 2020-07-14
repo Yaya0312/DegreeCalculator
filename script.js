ebae4abc43d78dec3319107a1911ba7d961dcd7c
Yannis Yahi <yahiyanis@gmail.com>
2020-07-14 20:17:32 +0200

    v1

--- script.js
+++ script.js
@@ -1,33 +1,77 @@
 'use strict'
 
-const maters = ['MPOO2','ALGO3','SE','TL','ANG','POP3'];
+// tools function
+const getsNodes = (x) => document.querySelectorAll(x);
+const zip = (a1, a2) => a1.map((x, i) => [x, a2[i]]); // a[int], b[int] -> [[a[0],b[0]]...,[a[n],b[n]]]
+const sum = (arr) => arr.reduce((a,c) => a + c); // a[int] -> (a[0] + ... + a[n])
+const toInt = (arr) => arr.map(x => +x);
 
-function res1() {
-    const getsClass = (x) => document.getElementsByClassName(x);
-    let names = Array.from(getsClass('name')).map( x => x.innerHTML);
-    let notes = Array.from(getsClass('note')).map( x => x.value);
-    let coefs = Array.from(getsClass('coef')).map( x => x.value);
-    let zip = (a1, a2) => a1.map((x, i) => [x, a2[i]]); 
-    let res = zip(notes, coefs).map( x => x[0] * x[1]);
-    res = res.reduce((a,c) => a + c);
-    document.getElementById("resultSemestre1").value = res;
-    if (res < 10) {
-        document.getElementById("semestre1").classList.replace("valid","notValid");
-    }
-    if (res > 10) {
-        document.getElementById("semestre1").classList.replace("notValid","valid");
+// data
+const matersS1 = ['MPOO2','ALGO3','SE','TL','ANG','POP3'];
+const matersS2 = ['IHM','Réseau','Compilation','Calcul Symbolique','ANG6','APP INFO','Stage'];
+
+function refresh(sem) {
+    //semester
+    const notes = Array.from(getsNodes(`#${sem} .note`)).map( x => x.value);
+    const coefs = Array.from(getsNodes(`#${sem} .coef`)).map( x => x.value);
+    const resultSemestre = sum(zip(notes, coefs).map(x => x[0] * x[1]));
+    const sumCoef = sum(coefs.map(x=>Number(x)));
+    document.getElementById("note" + sem).value = resultSemestre / sumCoef;
+    //final
+    const notesSemestre = Array.from(getsNodes(".noteSemestre")).map(x => x.value);
+    const resulYear = sum(notesSemestre.map(x=>Number(x))) / notesSemestre.length;
+    document.getElementById("noteFinal").value = resulYear;
+    if (resulYear >= 10) {
+        document.getElementById("textResult").innerText = "Félicitations année validé : ";
+    } else {
+        document.getElementById("textResult").innerText = "Année non validé : ";
     }
 }
 
-function addMater(name) {
+// functions
+
+function add(sem) {
+  addMater(" ",sem);
+}
+
+// components
+
+function addSemestre(n) {
+    const sem = `
+    <table class="semestre">
+    <caption>Semestre ${n}</caption>
+    <thead>
+      <th>Matière</th>
+      <th>(Coef)</th>
+      <th>Note</th>
+    </thead>
+    <tbody id="s${n}" class="notValid"></tbody>
+    <tfoot>
+      <tr>
+        <td colspan="3" class="add" onclick="add('s${n}')";>Ajouter une matière</td>
+      </tr>
+      <tr>
+        <td colspan="2" class="textNote">Note du semestre : </td>
+        <td><input type="number" class="noteSemestre" id="notes${n}" onchange="refresh('${n}')"></td>
+      </tr>
+    </tfoot>
+  </table>`;
+  document.getElementById("semestres").insertAdjacentHTML( 'beforeend', sem);
+}
+
+function addMater(name,sem) {
     const mater = `
-    <li>
-        <h5 class="name" contenteditable>${name}</h5> :
-        <input type="text" class="note" onchange="res1()">
-        (<input type="text" class="coef" onchange="res1()">)
-    </li>`;
-    document.getElementById("materS1").insertAdjacentHTML( 'beforeend', mater);
+    <tr>
+        <td contenteditable >${name}</td>
+        <td><input type="number" class="coef" onchange="refresh('${sem}')"></td>
+        <td><input type="number" class="note" onchange="refresh('${sem}')"></td>
+    </tr>`;
+    document.getElementById(sem).insertAdjacentHTML( 'beforeend', mater);
 }
 
-maters.forEach(x => addMater(x));
+// main
 
+addSemestre(1);
+addSemestre(2);
+matersS1.forEach(x => addMater(x,'s1'));
+matersS2.forEach(x => addMater(x,'s2'));
